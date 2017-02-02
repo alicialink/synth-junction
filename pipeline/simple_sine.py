9f5d93e436be0158c44a147b6b2f0a724d474116
Alicia Key <alicia.key@galvanize.com>
2017-02-02 10:45:41 -0700

    Now writes the basic sine wave

--- pipeline/simple_sine.py
+++ pipeline/simple_sine.py
@@ -1,2 +1,16 @@
+import numpy as np
+from scipy.io.wavfile import write
+
 if __name__ == "__main__":
-    print "Hello"
+    sps = 44100
+    freq = 440
+    duration_s = 1.0
+
+    each_sample_number = np.arange(sps * duration_s)
+    waveform = np.sin(2 * np.pi * each_sample_number * freq / sps)
+
+    waveform_integers = np.int16(waveform * 32767)
+
+    write('test.wav', 44100, waveform_integers)
+
+    print "Wrote test.wav"
