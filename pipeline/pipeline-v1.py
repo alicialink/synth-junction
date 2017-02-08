1b69fcdfdb29e1ac18a1b3bb65e0acbf9fa10409
Alicia Key <alicia.key@galvanize.com>
2017-02-07 21:03:35 -0700

    ooooh great fun!

--- pipeline/pipeline-v1.py
+++ pipeline/pipeline-v1.py
@@ -36,21 +36,26 @@ class Pipeline:
 
 if __name__ == "__main__":
     fundamental = 432
+    duration = 10.0
+    vol_level = 0.17
+    sps = 44100
+    max_freq = 20000
+    max_harmomic = int(max_freq // fundamental) + 1
 
-    sine = Pipeline(44100, 3.0, 0.1)
+    sine = Pipeline(sps, duration, vol_level)
     sine.add_sines([fundamental])
     sine.normalize_to_max_vol()
     sine.write_to_wav_file("test-pipeline-sine.wav")
     print "Wrote the sine wave"
 
-    am_sine = Pipeline(44100, 10.0, 0.1)
+    am_sine = Pipeline(sps, duration, vol_level)
     am_sine.add_sines([fundamental])
     am_sine.amplitudeModulate(2)
     am_sine.normalize_to_max_vol()
     am_sine.write_to_wav_file("test-pipeline-AM-sine.wav")
     print "Wrote the AM sine wave"
 
-    am_am_sine = Pipeline(44100, 10.0, 0.1)
+    am_am_sine = Pipeline(sps, duration, vol_level)
     am_am_sine.add_sines([fundamental])
     am_am_sine.amplitudeModulate(3)
     am_am_sine.amplitudeModulate(1)
@@ -58,11 +63,18 @@ if __name__ == "__main__":
     am_am_sine.write_to_wav_file("test-pipeline-AM-AM-sine.wav")
     print "Wrote the AM AM sine wave"
 
-    max_freq = 20000
-    max_harmomic = int(max_freq // fundamental) + 1
     squarish_freqs = [h * fundamental for h in range(1, 10) if h == 1 or h % 2 == 1]
-    squarish = Pipeline(44100, 3.0, 0.1)
+    squarish = Pipeline(sps, duration, vol_level)
     squarish.add_sines(squarish_freqs)
     squarish.normalize_to_max_vol()
     squarish.write_to_wav_file("test-pipeline-squarish.wav")
     print "Wrote the squarish wave"
+
+    squarish_freqs = [h * fundamental for h in range(1, 10) if h == 1 or h % 2 == 1]
+    am_am_squarish = Pipeline(sps, duration, vol_level)
+    am_am_squarish.add_sines(squarish_freqs)
+    am_am_squarish.amplitudeModulate(3)
+    am_am_squarish.amplitudeModulate(1)
+    am_am_squarish.normalize_to_max_vol()
+    am_am_squarish.write_to_wav_file("test-pipeline-AM-AM-squarish.wav")
+    print "Wrote the AM AM squarish wave"
